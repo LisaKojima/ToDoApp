47580e030acbf22b570be03de7c9f5a5671a926f
Risa Kojima <risakojima@RisanoMacBook-Pro.local>
2020-01-26 16:47:32 +0900

    first commit

---
+++ TodoApp/CalendarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarViewController.swift
+//  TodoApp
+//
+//  Created by Risa Kojima on 2020/01/26.
+//  Copyright © 2020 Kojima Lisa. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TodoApp/ToDoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoViewController.swift
+//  TodoApp
+//
+//  Created by Risa Kojima on 2020/01/26.
+//  Copyright © 2020 Kojima Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
